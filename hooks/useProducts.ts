59b2e4b8516319ffd8712755763280270fd3a4d8
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-14 12:38:23 +0200

    added fetch products hook

---
+++ hooks/useProducts.ts
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useState } from 'react';
+import { Product } from '../types/state';
+import { useFocusEffect } from '@react-navigation/native';
+import { sleep } from '../helpers/util';
+
+const key = 'products';
+
+export const useProducts = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<unknown>();
+
+  const fetchData = useCallback(async (signal?: AbortSignal) => {
+    try {
+      setLoading(true);
+
+      await sleep(1500);
+
+      const response = products;
+
+      if (response && response.length) {
+        setProducts(response);
+        setLoading(false);
+      } else {
+        throw response;
+      }
+    } catch (err) {
+      console.warn(`${key} Error `, err);
+      setLoading(false);
+      setError(err);
+    }
+  }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      const controller = new AbortController();
+      const signal = controller.signal;
+      fetchData(signal);
+
+      return () => {
+        controller.abort();
+      };
+    }, [])
+  );
+
+  return { products, loading, error, refetch: fetchData };
+};
