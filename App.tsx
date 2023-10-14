dfc3ebefad41adb7889ae9f7e70ab4587e4ff8e4
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-14 12:39:19 +0200

    added navigation bottom

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { TabNavigation } from './navigations/TabNavigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <StatusBar style='auto' />
+
+        <TabNavigation />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
