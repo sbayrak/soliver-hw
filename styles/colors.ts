3ca0daf3da0c42d76625aba33ef7259a4c783310
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-14 12:25:12 +0200

    added general colors

---
+++ styles/colors.ts
@@ -0,0 +1,23 @@
+import { ViewStyle } from 'react-native';
+
+export const primary = '#043AD7';
+export const primaryLight = '#86CDF9';
+export const primaryDark = '#080F8C';
+
+export const backgroundColor = '#EDF1FC';
+
+export const transparentText = '#C9C9CB';
+
+export const gray = '#F4F4F4';
+export const cardBg = '#FFFFFF';
+
+export const cardShadow: ViewStyle = {
+  shadowColor: '#898989',
+  shadowOffset: {
+    width: 0,
+    height: 9,
+  },
+  shadowOpacity: 0.22,
+  shadowRadius: 9.22,
+  elevation: 12,
+};
---
+++ styles/styles.ts
@@ -0,0 +1,5 @@
+import { ViewStyle } from 'react-native';
+
+export const flex1: ViewStyle = {
+  flex: 1,
+};
