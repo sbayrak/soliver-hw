095b190e5f1c8515d463a12ff94d0b2c92cee3ad
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-14 12:23:38 +0200

    added static product

---
+++ data.ts
@@ -0,0 +1,23 @@
+import { Product } from './types/state';
+
+export const data: Product[] = [
+  {
+    name: 'Strickpullover aus Baumwolle',
+    colors: [
+      { code: '#472C4C', name: 'aubergine' },
+      { code: '#000080', name: 'navy' },
+      { code: '#414141', name: 'graphit' },
+      { code: '#000000', name: 'black' },
+    ],
+    pictures: [
+      { name: 'aubergine', src: 'strickpullover-aubergine.png' },
+      { name: 'navy', src: 'strickpullover-navy.png' },
+      {
+        name: 'graphit',
+        src: 'strickpullover-graphit.png',
+      },
+      { name: 'black', src: 'strickpullover-black.png' },
+    ],
+    price: '49,99 €',
+  },
+];
