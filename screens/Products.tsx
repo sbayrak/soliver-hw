12e69f89356698d0c00e6de1001cbb44dc1affa9
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-14 13:50:28 +0200

    added products screen

---
+++ screens/Products.tsx
@@ -0,0 +1,51 @@
+import { StyleSheet, View, ViewStyle } from 'react-native';
+import React, { useState } from 'react';
+import { useProducts } from '../hooks/useProducts';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import ProductCardBig from '../components/ProductCardBig';
+import { gray, primary } from '../styles/colors';
+import { TabsParamList, TabsProps } from '../navigations/TabNavigation';
+import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
+import ProductCard from '../components/ProductCard';
+import SkeletonProducts from '../components/SkeletonProducts';
+import Header from '../components/Header';
+import SearchBar from '../components/SearchBar';
+import DismissKeyboard from '../components/DismissKeyboard';
+import { useDebouncedSearch } from '../hooks/useDebouncedSearch';
+import { MotiView } from 'moti';
+import { useIsFocused } from '@react-navigation/native';
+import ProductsList from '../components/product/ProductsList';
+import { StatusBar } from 'expo-status-bar';
+import { flex1 } from '../styles/styles';
+
+type Props = BottomTabScreenProps<TabsParamList, 'Products'>;
+
+const Products = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <StatusBar style='dark' />
+      <SafeAreaView style={flex1}>
+        <DismissKeyboard>
+          <View style={styles.wrapper}>
+            <ProductsList navigation={props.navigation} />
+          </View>
+        </DismissKeyboard>
+      </SafeAreaView>
+    </View>
+  );
+};
+
+export default Products;
+
+type Styles = {
+  container: ViewStyle;
+  wrapper: ViewStyle;
+};
+
+const styles = StyleSheet.create<Styles>({
+  container: {
+    flex: 1,
+    backgroundColor: gray,
+  },
+  wrapper: { flex: 1, paddingHorizontal: 20 },
+});
