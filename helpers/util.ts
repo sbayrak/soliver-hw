3b1f84b8c2c093b91e6a11e7aba764c23300a50a
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-15 17:26:06 +0200

    added function for fetching assets

--- helpers/util.ts
+++ helpers/util.ts
@@ -1,3 +1,16 @@
+import { ImageSourcePropType } from 'react-native';
+
 export function sleep(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
+
+type Images =
+  | 'strickpullover-aubergine.png'
+  | 'strickpullover-black.png'
+  | 'strickpullover-graphit.png'
+  | 'strickpullover-navy.png';
+
+export const getAsset = (src: Images) => {
+  const path = '../assets/' + src;
+  return '../assets/strickpullover-aubergine.png';
+};
