617678176254e2745223c767e93af0d2cc8cf1c6
sbayrak <34121748+sbayrak@users.noreply.github.com>
2023-10-15 12:54:06 +0200

    added new products

--- data.ts
+++ data.ts
@@ -11,14 +11,45 @@ export const products: Product[] = [
       { code: '#000000', name: 'black' },
     ],
     pictures: [
-      { name: 'aubergine', src: 'strickpullover-aubergine.png' },
-      { name: 'navy', src: 'strickpullover-navy.png' },
+      {
+        name: 'aubergine',
+        src: require('./assets/strickpullover-aubergine.png'),
+      },
+      { name: 'navy', src: require('./assets/strickpullover-navy.png') },
       {
         name: 'graphit',
-        src: 'strickpullover-graphit.png',
+        src: require('./assets/strickpullover-graphit.png'),
       },
-      { name: 'black', src: 'strickpullover-black.png' },
+      { name: 'black', src: require('./assets/strickpullover-black.png') },
     ],
-    price: '49,99 €',
+    price: 49,
+  },
+  {
+    id: '1231231235',
+    name: 'Steppjacke',
+    colors: [
+      { code: '#5B6667', name: 'olivgrun' },
+      { code: '#000080', name: 'navy' },
+      { code: '#000000', name: 'black' },
+    ],
+    pictures: [
+      { name: 'navy', src: require('./assets/steppjacke-navy.png') },
+      { name: 'olivgrun', src: require('./assets/steppjacke-navy.png') },
+      { name: 'black', src: require('./assets/steppjacke-black.png') },
+    ],
+    price: 129,
+  },
+  {
+    id: '5343461342452',
+    name: 'Jacke aus Schaffell-Imitat',
+    colors: [
+      { code: '#F0E1CD', name: 'natur' },
+      { code: '#000080', name: 'navy' },
+    ],
+    pictures: [
+      { name: 'navy', src: require('./assets/jacke-navy.png') },
+      { name: 'natur', src: require('./assets/jacke-natur.png') },
+    ],
+    price: 129,
   },
 ];
